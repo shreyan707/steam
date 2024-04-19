8f0b775b856c5e57ed5d03e7a61a33fbea0609e0
Ankitdas28 <dasabhayakumar26@gmail.com>
2024-04-19 15:22:13 +0530

    added login page and forgot passowrd + steam logo

--- main.cpp
+++ main.cpp
@@ -1,24 +1,40 @@
 #include<iostream>
-#include<string.h>
+#include<string>
 #include<fstream>
 using namespace std ;
 
 void login();
 void signin();
 void forgot();
+void logo (string filename) {
+    string line = "";
+    ifstream steam;
+    steam.open("logo.txt");
+    if(steam.is_open())
+    {
+        while(getline(steam,line))
+        {
+            cout << line << endl;
+        }
+        steam.close();
+    }
+}
 
 int main()
 {
     int choise;
     cout<<"\t\t\t______________________________________________________________\n\n\n";
-    cout<<"\t\t\t                        Welcome to the login page             \n\n\n";
+    cout<<"\t\t\t                        Welcome to Steam                      \n\n\n";
+    string steamy = "logo.txt";
+    logo(steamy);
+    cout<<"\n\n\n";
     cout<<"\t\t\t_____________________        MENU       ______________________\n\n\n";
     cout<<"\n\n\n";
     cout<<"| Press 1 to LOGIN           |"<<endl;
     cout<<"| Press 2 to SIGNIN          |"<<endl;
     cout<<"| Press 3 to FORGOT PASSWORD |"<<endl;
     cout<<"| Press 4 to EXIT            |"<<endl;
-    cout<<"\n\t\t\t ENTER YOUR CHOICE : ";
+    cout<<"\n ENTER YOUR CHOICE : ";
     cin>>choise;
     cout<<endl;
 
@@ -51,6 +67,7 @@ int main()
 
  void login()
  {
+    int count;
   string userID, password, id, pass;
   system("cls");
   cout<<"\t\t\t Please enter the username and password :" <<endl;
@@ -61,7 +78,100 @@ int main()
 
   ifstream input("record.txt");
 
+  while(input>>id>>pass)
+  {
+    if(id==userID && pass==password)
+    {
+        count =1;
+        system("cls");
+
+    }
+  }
+  input.close();
+
+  if (count==1)
+  {
+    cout<<userID<<"\n Your LOGIN is successful \n Thanks for logging in ! \n";
+    main();
+  }
+  else{
+    cout<<"\n LOGIN ERROR \n Please heck your username and password \n";
+    main();
+  }
+
  }
 
+void signin()
+{
+string suserID,spassword,sid,spass;
+system("cls");
+cout<<"\t\t\t Enter the username : " ;
+cin>>suserID;
+cout<<"\t\t\t Enter the password : ";
+cin>>spassword;
 
+ofstream f1("records.txt",ios::app);
+f1<<suserID<<' '<<spassword<<endl;
+system("cls");
+cout<<"\n\t\t\t Registration successfull! \n";
+main();
+
+}
+
+void forgot()
+{
+    int option;
+    system("cls");
+    cout<<"\t\t\t FORGOT PASSWORD \n\n ";
+    cout<<"Press 1 to search your id by the username "<<endl;
+    cout<<" Press 2 to go back to the main menu\n\n"<<endl;
+    cout<<"\t\t\tEnter your choice : ";
+    cin>>option;
+    switch (option)
+    {
+        case 1:
+        {
+            int count = 0;
+            string fuserID,fID,fpass;
+            cout<<"\n\t\t\tEnter the username you remembered: ";
+            cin>>fuserID;
+
+            ifstream f2("records.txt");
+            while(f2>>fID>>fpass)
+            {
+                if(fID==fuserID)
+                {
+                    count = 1;
+                }
+            }
+            f2.close();
+
+            if(count==1)
+            {
+                cout<<"\n\n Your account is found \n " ;
+                cout<<"\n\n Your password is :\n" <<fpass;
+                main();
+            }
+
+            else
+            {
+                cout << "\n\t Sorry! your account is not found!\n";
+                main();
+
+            }
+            break;
+
+        }
+
+         case 2:
+            {
+               main();     
+              }
+
+            default:
+            cout<<"\t\t\t Wrong choice ! Please try again " <<endl;
+            forgot();
+    
+    }
+}
 
