f1c751dc86da47674c224d12e2c6be67452aab2d
shreyan707 <shreyann13@gmail.com>
2024-04-19 13:58:12 +0530

    added game database and search feature

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
 #include<string>
+#include<fstream>
 using namespace std;
 int main()
 {
-    cout<<" hello world"<<endl;
+    string game_name;
+    fstream game_finder;
+    game_finder.open("games.txt", fstream::in);
+    cout << "Enter the game name : ";
+    cin >> game_name;
+    if(game_finder.is_open()){
+      string game,price,genre;
+      bool found = false;
+      while(!game_finder.eof()){
+        game_finder >> game >> price >> genre;
+        if( game == game_name){
+            cout << "The game you searched for is : " << game << " Price : " << price;
+            cout << " Genre : " << genre;
+            found = true;
+            break;
+        }
+      }
+      if (found==false){
+        cout << "Game is not in the database!";
+      }
+    }
+    else {
+        cout << "Games database is empty!";
+    }
+
 }
